a673fabb5e8a5510e61a0b832e3a1a1930cbb2c5
JH <wkdgns9797@naver.com>
2024-12-20 18:04:55 +0900

    login 버튼 css, 반응형 수정 12/20 장훈 (18:04)

--- src/main/moivo_react/src/containers/user/user_login.jsx
+++ src/main/moivo_react/src/containers/user/user_login.jsx
@@ -74,20 +74,31 @@ const user_login = () => {
                             <h1>Moivo</h1>
                         </Link>
                         <div className={signin['social-container']}>
-                            <button type="button" onClick={handleKakaoLogin} style={{ background: 'none', border: 'none', padding: 0, cursor: 'pointer' }} >
-                                <img src={kakaoLoginImage} alt="카카오 로그인"  style={{ width: '100%', height: '100%', cursor: 'pointer' }} />
-                            </button>
-                            <a href="#" className={signin.social}><i className="fab fa-google-plus-g"></i></a>
-                            <a href="#" className={signin.social}><i className="fab fa-linkedin-in"></i></a>
                         </div>
                         <span>If you don't want to sign up,<br/>or use your account instead.</span>
                         <input type="text" name="userId" value={formData.id} onChange={handleChange} onFocus={handleFocus} placeholder="ID" required/>
                         <input type="password" name="pwd" value={formData.pwd} onChange={handleChange} onFocus={handleFocus} placeholder="Password" required/>
                         {error && <div className={signin.error} dangerouslySetInnerHTML={{ __html: error }} />}
-                        <button type="submit">Sign In</button>
-                        <Link to="/user_signup">
-                            <button type="submit" className={signin.singupbtn}>Sign Up</button>
-                        </Link>
+                        <div className={signin.signBtn}>
+                            
+                            <button type="submit" className={signin.signinbtn}>Sign In</button>
+                            
+                            {/* 반응형-> 나타나는 회원가입 버튼 */}
+                            <Link to="/user_signup">
+                                <button type="submit" className={signin.singupbtn}>Sign Up</button>
+                            </Link> 
+                                
+                            <button type="button" className={signin.kakao_nomedia} onClick={handleKakaoLogin} style={{ background: 'none', border: 'none', padding: 0, cursor: 'pointer' }} >
+                                <img src={kakaoLoginImage} alt="카카오 로그인"  style={{ width: '100%', height: '100%', cursor: 'pointer' }} />
+                            </button>
+                        </div>
+
+                        {/* 반응형-> 나타나는 카카오 로그인 버튼 */}
+                        <div className={signin.kakao_media}>
+                            <button type="button" onClick={handleKakaoLogin} style={{ background: 'none', border: 'none', padding: 0, cursor: 'pointer' }} >
+                                <img src={kakaoLoginImage} alt="카카오 로그인"  style={{ width: '100%', height: '100%', cursor: 'pointer' }} />
+                            </button>
+                        </div>
                     </form>
                 </div>
 
