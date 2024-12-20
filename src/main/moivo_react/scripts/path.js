5826bcf9c1e6f34c35b3c24d83eb71468766978a
Your Name <your.email@example.com>
2024-12-20 15:13:29 +0900

    배포시 fortk syn

--- src/main/moivo_react/scripts/path.js
+++ src/main/moivo_react/scripts/path.js
@@ -1,4 +1,4 @@
 export const PATH = {
-    SERVER: 'http://localhost:8080', // 서버 주소
-    CLIENT: 'http://localhost:5173' // 클라이언트 주소
+    SERVER: 'http://localhost:8080', // 배포시 백엔드 서버 주소 http://211.188.63.191:8080
+    CLIENT: 'http://localhost:5173' // 배포시 프론트엔드 클라이언트 주소 http://211.188.63.191:5173
 };
