c92e018193b1256b558a12d404da39df6a51bfe4
Your Name <your.email@example.com>
2024-12-20 17:41:29 +0900

    배포오류수정5

--- src/main/moivo_react/scripts/path.js
+++ src/main/moivo_react/scripts/path.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 export const PATH = {
     SERVER: process.env.NODE_ENV === 'production' 
-        ? 'http://211.188.63.191:8080' // 배포 환경
+        ? 'http://211.188.63.191:9090' // 배포 환경
         : 'http://localhost:8080',    // 로컬 개발 환경
     CLIENT: process.env.NODE_ENV === 'production' 
         ? 'http://211.188.63.191:5173' // 배포 환경
