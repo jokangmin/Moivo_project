a433db4d5b7155a8ea01180949dfdee910bcd15d
Your Name <your.email@example.com>
2024-12-23 09:29:57 +0900

    에러파일 재생성

---
+++ src/main/java/com/example/demo/store/repository/ReviewRepository.java
@@ -0,0 +1,31 @@
+package com.example.demo.store.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.store.entity.ReviewEntity;
+
+import jakarta.transaction.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<ReviewEntity, Integer>, ReviewRepositoryCustom  {
+    
+    public Page<ReviewEntity> findByProductEntityId(int productId, Pageable pageable);
+    
+    // 특정 사용자의 리뷰 조회
+    public Page<ReviewEntity> findByUserEntityIdAndProductEntityId(int userId, int productId, Pageable pageable);
+    
+    // 사용자의 모든 리뷰 조회 메서드 추가
+    public Page<ReviewEntity> findByUserEntityId(int userId, Pageable pageable);
+    
+    // 결제상세ID로 리뷰 찾기
+    public Optional<ReviewEntity> findByPaymentDetailEntityId(int paymentDetailId);
+
+}
