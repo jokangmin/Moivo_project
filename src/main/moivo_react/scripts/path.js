b088d261ce09031a186d78d64dfc6a2e55fd9be8
Your Name <your.email@example.com>
2024-12-30 14:26:46 +0900

    상품목록 쿼리스트링적용 12.30 14:26

--- src/main/moivo_react/scripts/path.js
+++ src/main/moivo_react/scripts/path.js
@@ -8,6 +8,6 @@ export const PATH = {
     // CLIENT: 'http://101.79.9.149' // 클라이언트 주소
 
     // 도메인
-    SERVER: 'https://moivo.shop', // 서버 주소 !
+    SERVER: 'https://moivo.shop', // 서버 주소
     CLIENT: 'https://moivo.shop' // 클라이언트 주소
 };
