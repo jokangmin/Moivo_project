9c11c85a1dff6dd6ada3ccda48754873f2c0747e
JH <wkdgns9797@naver.com>
2025-01-02 12:31:52 +0900

    hoon 브랜치 작업물 + test 브랜치 2025/01/02 (12:31)

--- src/main/moivo_react/src/components/Banner/banner.jsx
+++ src/main/moivo_react/src/components/Banner/banner.jsx
@@ -9,7 +9,7 @@ import cartIcon from '../../assets/image/cart.png';
 const Banner = () => {
   const navigate = useNavigate();
   const { isAuthenticated, logout, tokenExpiration, isAdmin } = useContext(AuthContext);
-  //2024-12-11 디버그 확인 장훈s
+  //2024-12-11 디버그 확인 장훈
   console.log('isAdmin:', isAdmin);
 
   useEffect(() => {
